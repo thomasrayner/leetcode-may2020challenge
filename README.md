3053fee2f68def8d85f86d3d39574f97257600d8
Thomas Rayner <thmsrynr@outlook.com>
2020-05-01 08:48:31 -0700

    day01

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# leetcode-may2020challenge
+# May LeetCoding Challenge
+
+From [LeetCode](https://leetcode.com/explore/featured/card/may-leetcoding-challenge/)
+
+The description:
+
+> This Challenge is beginner-friendly and available to both Premium and non-Premium users. It consists of 31 daily problems over the month of May. A problem is added here each day, and you have 24 hours to make a valid submission for it in order to be eligible for rewards.
+
+## Challenges
+
+| Day | Challenge | Notes |
+|-----|-----------|-------|
+| 01 | [First Bad Version](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/) | Interactive problem that doesn't run locally. Identified this solution right away and was able to complete it without references! |
---
+++ day01/day01.cs
@@ -0,0 +1,35 @@
+﻿// From: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
+// Interactive problem that doesn't run outside leetcode
+
+using System;
+
+namespace day01
+{
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+    public class Solution : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            int start = 0;
+            int end = n;
+
+            while (start < end)
+            {
+                int middle = start + (end - start) / 2;
+
+                if (IsBadVersion(middle))
+                {
+                    end = middle;
+                }
+                else
+                {
+                    start = middle + 1;
+                }
+            }
+
+            return start;
+        }
+    }
+}
