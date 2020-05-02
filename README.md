7d38ee8fdbe7cbcad31892b922c5dde56d0bd483
Thomas Rayner <thmsrynr@outlook.com>
2020-05-02 15:29:50 -0700

    day02

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ The description:
 
 | Day | Challenge | Notes |
 |-----|-----------|-------|
-| 01 | [First Bad Version](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/) | Interactive problem that doesn't run locally. Identified this solution right away and was able to complete it without references! |
+| 01 | [First Bad Version](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/) | Interactive problem that doesn't run locally. Binary search. |
+| 02 | [Jewels and Stones](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/) | |
+
---
+++ day02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Jewels: ");
+            string jewels = Console.ReadLine();
+            Console.Write("Stones: ");
+            string stones = Console.ReadLine();
+
+            Solution sol = new Solution();
+            int final = sol.NumJewelsInStones(jewels, stones);
+            Console.WriteLine("Final: " + final.ToString());
+        }
+    }
+
+    public class Solution
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            char[] jewels = J.ToCharArray();
+            int jewelCount = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (jewels.Contains(S[i]))
+                {
+                    jewelCount++;
+                }
+            }
+
+            return jewelCount;
+        }
+    }
+}
