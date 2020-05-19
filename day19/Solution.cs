8cd856600b849c8597b5be7caed8112b5ba433ce
Thomas Rayner <thmsrynr@outlook.com>
2020-05-19 07:56:18 -0700

    day19

---
+++ day19/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day19
+{
+    public class StockSpanner
+    {
+        public Stack<(int price, int count)> StockStack { get; set; }
+
+        public StockSpanner()
+        {
+            StockStack = new Stack<(int p, int c)>();
+        }
+
+        public int Next(int price)
+        {
+            int dayCount = 1;
+
+            while (StockStack.Count > 0 && price >= StockStack.Peek().price)
+            {
+                dayCount += StockStack.Pop().count;
+            }
+
+            StockStack.Push((price, dayCount));
+            return dayCount;
+        }
+    }
+
+    /**
+     * Your StockSpanner object will be instantiated and called as such:
+     * StockSpanner obj = new StockSpanner();
+     * int param_1 = obj.Next(price);
+     */
+}
