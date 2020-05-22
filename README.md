260dcde63faae8a4659cbcdb616d5e9d40f5406e
Thomas Rayner <thmsrynr@outlook.com>
2020-05-22 10:33:42 -0700

    day22

--- README.md
+++ README.md
@@ -31,3 +31,4 @@ The description:
 | 19 | [Online Stock Spanner](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3334/) | Interactive problem that doesn't run locally. Stacks. |
 | 20 | [Kth Smallest Element in a BST](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3335/) | Interactive problem that doesn't run locally. BST, binary search tree, DFS, depth-first search, recursion, in-order traversal. |
 | 21 | [Count Square Submatrices with All Ones](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3336/) | Dynamic programming. |
+| 22 | [Sort Characters By Frequency](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3337/) | Frequency sorting. |
---
+++ day22/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace day22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string s = Console.ReadLine();
+
+            Solution sol = new Solution();
+            string final = sol.FrequencySort(s);
+            Console.WriteLine("Final: " + final);
+        }
+    }
+
+    public class Solution
+    {
+        public string FrequencySort(string s)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (dict.TryGetValue(c, out int count))
+                {
+                    dict[c] = count + 1;
+                }
+
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+
+            var sortedDict = from entry in dict orderby entry.Value descending select entry;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<char, int> entry in sortedDict)
+            {
+                for (int i = 0; i < entry.Value; i++)
+                {
+                    sb.Append(entry.Key);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
