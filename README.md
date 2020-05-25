8abe1ecf3c84db4b9b6840120590457d69d2c5db
Thomas Rayner <thmsrynr@outlook.com>
2020-05-25 12:45:39 -0700

    day25

--- README.md
+++ README.md
@@ -33,3 +33,5 @@ The description:
 | 21 | [Count Square Submatrices with All Ones](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3336/) | Dynamic programming. |
 | 22 | [Sort Characters By Frequency](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3337/) | Frequency sorting. |
 | 23 | [Interval List Intersections](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3338/https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3338/) | Find intersections of using pointers. |
+| 24 | [Construct Binary Search Tree from Preorder Traversal](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3339/) | BST. |
+| 25 | [UncrossedLines](https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3340/) | Dynamic programming. |
---
+++ day25/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace day25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter A: ");
+            int[] A = Console.ReadLine().Split(',').Select(n => int.Parse(n)).ToArray();
+            Console.Write("Enter B: ");
+            int[] B = Console.ReadLine().Split(',').Select(n => int.Parse(n)).ToArray();
+
+            Solution sol = new Solution();
+            int final = sol.MaxUncrossedLines(A, B);
+            Console.WriteLine("Final: " + final.ToString());
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxUncrossedLines(int[] A, int[] B)
+        {
+            int[,] dp = new int[A.Length + 1, B.Length + 1];
+            for (int i = 1; i <= A.Length; i++)
+            {
+                for (int j = 1; j <=B.Length; j++)
+                {
+                    if (A[i - 1] == B[j - 1])
+                    {
+                        dp[i,j] = dp[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+
+            return dp[A.Length, B.Length];
+        }
+    }
+}
